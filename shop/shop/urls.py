c3c736101d6f84b3056060365fdca405cd294d9b
Monkey8bit <de_construct@mail.ru>
2022-01-16 21:58:15 +0400

    Fixed urls and href in templates

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -6,6 +6,6 @@ from mainapp import views as mainapp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", mainapp.index),
-    path("contacts/", mainapp.contact),
-    path("products/", mainapp.product),
+    path("contact/", mainapp.contact),
+    path("product/", mainapp.product),
 ]
