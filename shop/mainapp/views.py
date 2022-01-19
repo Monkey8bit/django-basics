41301e3ef55945c8efd67ff19ac96a7a80f2c2da
Monkey8bit <de_construct@mail.ru>
2022-01-20 00:16:26 +0400

    lesson_2

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,11 +1,31 @@
+from unicodedata import name
 from django.shortcuts import render
 
 
+menu = [
+    {"name": "домой", "link": "index"},
+    {"name": "продукты", "link": "product"},
+    {"name": "контакты", "link": "contact"},
+]
+
 def index(request):
-    return render(request, "mainapp/index.html")
+    return render(request, "mainapp/index.html", context={
+        "menu": menu
+    })
 
 def product(request):
-    return render(request, "mainapp/products.html")
+    products = [
+        {"name": "Отличный стул", "link": "static/img/product-11.jpg"},
+        {"name": "Отличный стул", "link": "static/img/product-21.jpg"},
+        {"name": "Отличный стул", "link": "static/img/product-21.jpg"},
+        
+    ]
+    return render(request, "mainapp/products.html", context={
+        "menu": menu,
+        "products": products
+    })
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    return render(request, "mainapp/contact.html", context={
+        "menu": menu
+    })
