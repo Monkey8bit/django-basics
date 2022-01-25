33706b440737c1ee5220dcc1e395c194cfc9dd00
Monkey8bit <de_construct@mail.ru>
2022-01-25 13:03:38 +0400

    lesson_3

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,31 +1,42 @@
-from unicodedata import name
 from django.shortcuts import render
+from .models import Product, ProductCategory
 
 
 menu = [
-    {"name": "домой", "link": "index"},
-    {"name": "продукты", "link": "product"},
-    {"name": "контакты", "link": "contact"},
+    {"name": "home", "link": "index"},
+    {"name": "product", "link": "product:index"},
+    {"name": "contacts", "link": "contact"},
 ]
 
 def index(request):
-    return render(request, "mainapp/index.html", context={
-        "menu": menu
+    product = Product.objects.all()
+
+    return render(request,
+     "mainapp/index.html",
+      context={
+        "title": "Главная",
+        "menu": menu,
+        "products": product,
     })
 
-def product(request):
+def product(request, pk=None):
     products = [
         {"name": "Отличный стул", "link": "static/img/product-11.jpg"},
         {"name": "Отличный стул", "link": "static/img/product-21.jpg"},
         {"name": "Отличный стул", "link": "static/img/product-21.jpg"},
         
     ]
+    category = ProductCategory.objects.all()
     return render(request, "mainapp/products.html", context={
+        "title": "Продукты",
         "menu": menu,
-        "products": products
+        "products": products,
+        "categories": category,
+        "pk": pk,
     })
 
 def contact(request):
     return render(request, "mainapp/contact.html", context={
+        "title": "Контакты",
         "menu": menu
     })
