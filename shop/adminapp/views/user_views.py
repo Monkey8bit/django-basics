bb4463e26295ef8017cfa91970f9850e0f691a78
Monkey8bit <de_construct@mail.ru>
2022-02-15 15:56:47 +0400

    lesson_8

--- shop/adminapp/views/user_views.py
+++ shop/adminapp/views/user_views.py
@@ -1,82 +1,66 @@
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from authapp.models import ShopUser
 from django.contrib.auth.decorators import user_passes_test
-from django.urls import reverse
-from authapp.forms import ShopUserRegisterForm
-from adminapp.forms import ShopUserAdminEditForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.utils.decorators import method_decorator
+
+from adminapp.forms import ShopUserAdminEditForm, ShopUserAdminCreateForm
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def users(request):
-    users_list = ShopUser.objects.all()
+class UserListView(ListView):
+    model = ShopUser
+    template_name = "adminapp/user/users.html"
 
-    return render(
-        request,
-        "adminapp/user/users.html",
-        context={
-            "title": "Admin/users",
-            "users": users_list,
-        },
-    )
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def create_user(request):
-    if request.method == "POST":
-        user_form = ShopUserRegisterForm(request.POST, request.FILES)
-        if user_form.is_valid():
-            user_form.save()
-            return HttpResponseRedirect(reverse("adminapp:users"))
-    else:
-        user_form = ShopUserRegisterForm()
+class UserCreateView(CreateView):
+    model = ShopUser
+    template_name = "adminapp/user/update_user.html"
+    form_class = ShopUserAdminCreateForm
+    success_url = reverse_lazy("adminapp:users")
 
-    return render(
-        request,
-        "adminapp/user/update_user.html",
-        context={
-            "title": "Admin/user/create",
-            "update_form": user_form,
-        },
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Admin/create"
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def update_user(request, pk):
-    edit_user = get_object_or_404(ShopUser, pk=pk)
-    if request.method == "POST":
-        edit_form = ShopUserAdminEditForm(
-            request.POST, request.FILES, instance=edit_user
-        )
+class UserUpdateView(UpdateView):
+    model = ShopUser
+    template_name = "adminapp/user/update_user.html"
+    form_class = ShopUserAdminEditForm
+    success_url = reverse_lazy("adminapp:users")
 
-        if edit_form.is_valid():
-            edit_form.save()
-            return HttpResponseRedirect(
-                reverse("adminapp:update_user", args=[edit_user.pk])
-            )
-    else:
-        edit_form = ShopUserAdminEditForm(instance=edit_user)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_name = self.object.username
+        context["title"] = f"Admin/{user_name}/edit"
+        return context
 
-    return render(
-        request,
-        "adminapp/user/update_user.html",
-        context={"title": "Admin/user/edit", "update_form": edit_form},
-    )
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def delete_user(request, pk):
-    user = get_object_or_404(ShopUser, pk=pk)
+class UserDeleteView(DeleteView):
+    model = ShopUser
+    template_name = "adminapp/user/delete_user.html"
+    success_url = reverse_lazy("adminapp:users")
 
-    if request.method == "POST":
-        user.is_active = False
-        user.save()
-        return HttpResponseRedirect(reverse("adminapp:users"))
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-    return render(
-        request,
-        "adminapp/user/delete_user.html",
-        context={
-            "title": "Admin/user/delete",
-            "user_to_delete": user,
-        },
-    )
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 import mainapp.views as mainapp
 
 
@@ -6,6 +7,7 @@ app_name = "mainapp"
 
 urlpatterns = [
     path("", mainapp.product, name="index"),
-    path("<int:pk>/", mainapp.category, name="category"),
+    path("<int:pk>/", mainapp.product, name="category"),
+    path("<int:pk>/page/<int:page>/", mainapp.product, name="page"),
     path("product/<int:pk>/", mainapp.product_page, name="product"),
 ]
